3ad26fcb3dd2106893d7c5b12b308031b208d16b
ishland <ishlandmc@yeah.net>
2023-06-22 16:37:31 +0800

    feat: mixin audit

--- src/main/java/com/ishland/earlyloadingscreen/TheMod.java
+++ src/main/java/com/ishland/earlyloadingscreen/TheMod.java
@@ -3,6 +3,14 @@ package com.ishland.earlyloadingscreen;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.service.MixinService;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TheMod implements ModInitializer {
 
@@ -10,6 +18,49 @@ public class TheMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        auditMixins();
+    }
 
+    private static void auditMixins() {
+        Logger auditLogger = LoggerFactory.getLogger("EarlyLoadingScreen Mixin Audit");
+        try {
+            final Class<?> transformerClazz = Class.forName("org.spongepowered.asm.mixin.transformer.MixinTransformer");
+            if (transformerClazz.isInstance(MixinEnvironment.getCurrentEnvironment().getActiveTransformer())) {
+                final Field processorField = transformerClazz.getDeclaredField("processor");
+                processorField.setAccessible(true);
+                final Object processor = processorField.get(MixinEnvironment.getCurrentEnvironment().getActiveTransformer());
+                final Class<?> processorClazz = Class.forName("org.spongepowered.asm.mixin.transformer.MixinProcessor");
+                final Field configsField = processorClazz.getDeclaredField("configs");
+                configsField.setAccessible(true);
+                final List<?> configs = (List<?>) configsField.get(processor);
+                final Class<?> configClazz = Class.forName("org.spongepowered.asm.mixin.transformer.MixinConfig");
+                final Method getUnhandledTargetsMethod = configClazz.getDeclaredMethod("getUnhandledTargets");
+                getUnhandledTargetsMethod.setAccessible(true);
+                Set<String> unhandled = new HashSet<>();
+                for (Object config : configs) {
+                    final Set<String> unhandledTargets = (Set<String>) getUnhandledTargetsMethod.invoke(config);
+                    unhandled.addAll(unhandledTargets);
+                }
+                final LoadingScreenManager.RenderLoop renderLoop = LoadingScreenManager.windowEventLoop.renderLoop;
+                try (LoadingScreenManager.RenderLoop.ProgressHolder progressHolder = renderLoop != null ? renderLoop.new ProgressHolder() : null) {
+                    int index = 0;
+                    int total = unhandled.size();
+                    for (String s : unhandled) {
+                        if (progressHolder != null) {
+                            progressHolder.update(String.format("Loading class (%d/%d): %s ", index, total, s));
+                        }
+                        MixinService.getService().getClassProvider().findClass(s, false);
+                        index ++;
+                    }
+                }
+                for (Object config : configs) {
+                    for (String unhandledTarget : (Set<String>) getUnhandledTargetsMethod.invoke(config)) {
+                        auditLogger.error("{} is already classloaded", unhandledTarget);
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException("Failed to audit mixins", t);
+        }
     }
 }
