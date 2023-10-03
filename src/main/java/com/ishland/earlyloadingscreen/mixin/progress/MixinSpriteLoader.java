76e62f3c1bb5be9c157ad4cf968b775b44ff18d6
ishland <ishlandmc@yeah.net>
2023-10-03 18:20:34 +0800

    build: 1.20.2

--- src/main/java/com/ishland/earlyloadingscreen/mixin/progress/MixinSpriteLoader.java
+++ src/main/java/com/ishland/earlyloadingscreen/mixin/progress/MixinSpriteLoader.java
@@ -3,6 +3,7 @@ package com.ishland.earlyloadingscreen.mixin.progress;
 import com.ishland.earlyloadingscreen.util.ProgressUtil;
 import net.minecraft.client.texture.SpriteContents;
 import net.minecraft.client.texture.SpriteLoader;
+import net.minecraft.client.texture.SpriteOpener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,13 +15,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Mixin(SpriteLoader.class)
 public class MixinSpriteLoader {
 
     @Inject(method = "loadAll", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private static void captureSpriteLoad(List<Supplier<SpriteContents>> sources, Executor executor, CallbackInfoReturnable<CompletableFuture<List<SpriteContents>>> cir, List<CompletableFuture<SpriteContents>> list) {
+    private static void captureSpriteLoad(SpriteOpener opener, List<Function<SpriteOpener, SpriteContents>> sources, Executor executor, CallbackInfoReturnable<CompletableFuture<List<SpriteContents>>> cir, List<CompletableFuture<SpriteContents>> list) {
         ProgressUtil.createProgress(list, cir.getReturnValue(), "sprites");
     }
 
